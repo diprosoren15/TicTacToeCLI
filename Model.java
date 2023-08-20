1be194451d68279d9225b68438b80e101849627b
Dipro Soren <diprosoren15@gmail.com>
2023-08-21 04:08:10 +0530

    wip: Worked out basic structure of the game

--- Model.java
+++ Model.java
@@ -1,44 +1,61 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 enum XO {
     O,
     X;
-    public static String toString(XO xo) {
+    public static String toString(@Nullable XO xo) {
         if(xo == null)
             return "_";
 
-        switch (xo) {
-            case O : return "O";
-            case X : return "X";
-            default : return " ";
-        }
+        return switch (xo) {
+            case O -> "O";
+            case X -> "X";
+        };
     }
 }
 
 enum Outcome {
     WIN,
     DRAW;
+    public String toString(XO activePlayer) {
+        return switch (this) {
+            case WIN -> activePlayer.toString()+ " wins!";
+            case DRAW -> "Its a draw!";
+        };
+    }
+
 }
 
 public class Model {
-    XO[][] grid = new XO[3][3];
-    XO activePLayer = XO.O;
+    @Nullable XO @NotNull [] grid = new XO[9];
+    XO activePlayer = XO.O;
+
+    void togglePlayer() {
+        switch (activePlayer) {
+            case X -> activePlayer = XO.O;
+            case O -> activePlayer = XO.X;
+        }
+    }
 
     public String toString() {
         StringBuilder frame = new StringBuilder();
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                XO xo = grid[i][j];
-                frame.append(XO.toString(xo));
-                frame.append(" ");
-                if (j == 2) {
-                    frame.append("\n");
-                }
+            XO xo = grid[i];
+            frame.append(XO.toString(xo));
+            frame.append(" ");
+            if ((i+1)%3 == 0) {
+                frame.append("\n");
             }
         }
+        frame.append("\n");
+        frame.append(activePlayer.toString());
+        frame.append("'s move: ");
         return frame.toString();
     }
 
     public static void main(String[] args) {
         Model model = new Model();
-        System.out.println(model.toString());
+        assert model.toString().equals("_ _ _ \n_ _ _ \n_ _ _ \n\nO's move : ") : "Model.toString failed";
     }
 }
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,40 @@
+import org.jetbrains.annotations.Nullable;
+import java.util.Scanner;
+
 public class TicTacToe {
     Model model = new Model();
-    XO xo = XO.O;
-    boolean checkDraw() {
 
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Nullable Integer getPlayerMove() {
+        return 1;
+    }
+
+    Integer getComputerMove() {
+        return 2;
+    }
+    @Nullable Integer getMove() {
+        return switch (model.activePlayer) {
+            case O -> getPlayerMove();
+            case X -> getComputerMove();
+        };
+    }
+
+    void playMove(Integer move) {
+        XO xo = switch (model.activePlayer) {
+            case X -> XO.X;
+            case O -> XO.O;
+        };
+        model.grid[move-1] = xo;
+    }
+    boolean checkDraw() {
+        return false;
     }
 
     boolean checkWin() {
-
+        return false;
     }
-    Outcome checkOutcome() {
-
+    @Nullable Outcome checkOutcome() {
         if (checkWin()) {
             return Outcome.WIN;
         }
@@ -19,8 +44,28 @@ public class TicTacToe {
         return null;
     }
 
+    void run() {
+        System.out.println("WELCOME TO TIC TAC TOE");
+
+        while (true) {
+            System.out.println(this.model.toString());
+            Integer move = getMove();
+            if(move == null) {
+                System.out.println("Invalid Move. Please retry!");
+                continue;
+            }
+            playMove(move);
+            Outcome outcome = checkOutcome();
+            if (outcome != null) {
+                System.out.println(outcome.toString(model.activePlayer));
+                break;
+            }
+            model.togglePlayer();
+        }
+    }
 
     public static void main(String[] args) {
-
+        TicTacToe tictactoe = new TicTacToe();
+        tictactoe.run();
     }
 }
